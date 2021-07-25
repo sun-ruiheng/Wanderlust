d7e9121cd2ba90f8256322718229dba236ca6bc1
Sun Ruiheng <sunruiheng2001@gmail.com>
2021-07-26 00:20:20 +0800

    changed F to Celsius

--- public/helpers.js
+++ public/helpers.js
@@ -10,9 +10,9 @@ const createVenueHTML = (name, location, iconSource) => {
   const createWeatherHTML = (currentDay) => {
     console.log(currentDay)
     return `<h2>${weekDays[(new Date()).getDay()]}</h2>
-          <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
+          <h2>Temperature: ${kelvinToCelsius(currentDay.main.temp)}&deg;F</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
         <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
   }
   
-  const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
+  const kelvinToCelsius = k => (k - 273.15).toFixed(0);
